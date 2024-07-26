5a212aaff88f19c2002c39b6504634e83c9f5744
VedantRaval3011 <vedantraval30@gmail.com>
2024-07-26 13:39:34 +0530

    Created user model , installed multer

---
+++ src/models/user.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    birthdate: {
+      type: Date,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("userSchema", userSchema);
